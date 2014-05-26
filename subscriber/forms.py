694a6c0ca74e85e6337f4f3d39ba237a4040c41c
jayakrishnandingit <jayakrishnandamodaran@gmail.com>
2014-05-26 22:56:35 +0530

    minor change in email field

--- subscriber/forms.py
+++ subscriber/forms.py
@@ -5,4 +5,4 @@ from models import Subscriber
 class SubscriberCreationForm(forms.Form):
 	first_name = StrippedCharField(widget = forms.TextInput(attrs = {'class' : 'form-control', 'ng-model':'form_data.first_name', 'ng-required' : 'true'}), required = True)
 	last_name = StrippedCharField(widget = forms.TextInput(attrs = {'class' : 'form-control', 'ng-model':'form_data.last_name', 'ng-required' : 'true'}), required = True)
-	email = forms.EmailField(widget = forms.EmailInput(attrs = {'class' : 'form-control', 'ng-model':'form_data.email'}), required = True)
+	email = forms.EmailField(widget = forms.EmailInput(attrs = {'class' : 'form-control', 'ng-model':'form_data.email', 'ng-required' : 'true'}), required = True)
