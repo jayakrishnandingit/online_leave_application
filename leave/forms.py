a4303a9de34ccc20adb0d884e03be276193d85b3
jayakrishnandamodaran <jayakrishnan.d@uroleaf.com>
2014-05-30 17:26:48 +0530

    home page leave form set

--- leave/forms.py
+++ leave/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from models import Leave
+from ola.common.forms import StrippedCharField
 
 class LeaveForm(forms.ModelForm):
 	leave_type = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control', 'ng-model':'leave.type', 'ng-required':'true'}), required=True)
+	approvers = StrippedCharField(widget=forms.TextInput(attrs={'placeholder':'Send Request To', 'class':'form-control', 'ng-model':'leave.approver', 'ng-required':'true'}), required=True)
 	class Meta:
 		model = Leave
 		exclude = ['type_of_leave', 'requester', 'approver', 'status', 'created_on']
